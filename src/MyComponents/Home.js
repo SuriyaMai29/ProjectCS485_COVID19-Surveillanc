49a32b688ed5623fbc427e6951bbf83bf9896f2f
unknown <suriya.mee29@gmail.com>
2020-05-17 20:18:07 +0700

    Appbar version1

---
+++ src/MyComponents/Home.js
@@ -0,0 +1,112 @@
+import React from 'react'
+import AppBar from './AppBar'
+import '../MyCss/Home.css'
+import Card from '../Card/Card'
+import CardHeader from '../Card/CardHeader'
+import CardIcon from '../Card/CardIcon'
+import Icon from "@material-ui/core/Icon";
+import CardFooter  from '../Card/CardFooter';
+import styles from "../Card/jss/material-dashboard-react/views/dashboardStyle.js";
+import Danger from '../Card/Danger';
+import Warning from "@material-ui/icons/Warning";
+import { makeStyles } from "@material-ui/core/styles";
+import GridItem from "./Grid/GridItem";
+import GridContainer from "./Grid/GridContainer";
+import Store from "@material-ui/icons/Store";
+import DateRange from "@material-ui/icons/DateRange";
+import LocalOffer from "@material-ui/icons/LocalOffer";
+import Update from "@material-ui/icons/Update";
+import Accessibility from "@material-ui/icons/Accessibility";
+
+const useStyles = makeStyles(styles);
+
+export default class Home extends React.Component{
+    render(){
+        const classes = useStyles();
+        return(<> 
+            <AppBar></AppBar>
+            <GridContainer>
+        <GridItem xs={12} sm={6} md={3}>
+
+
+          <Card>
+            <CardHeader color="warning" stats icon>
+              <CardIcon color="warning">
+                <Icon>content_copy</Icon>
+              </CardIcon>
+              <p className={classes.cardCategory}>Used Space</p>
+              <h3 className={classes.cardTitle}>
+                49/50 <small>GB</small>
+              </h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <Danger>
+                  <Warning />
+                </Danger>
+                <a href="#pablo" onClick={e => e.preventDefault()}>
+                  Get more space
+                </a>
+              </div>
+            </CardFooter>
+          </Card>
+
+
+
+          
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="success" stats icon>
+              <CardIcon color="success">
+                <Store />
+              </CardIcon>
+              <p className={classes.cardCategory}>Revenue</p>
+              <h3 className={classes.cardTitle}>$34,245</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <DateRange />
+                Last 24 Hours
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="danger" stats icon>
+              <CardIcon color="danger">
+                <Icon>info_outline</Icon>
+              </CardIcon>
+              <p className={classes.cardCategory}>Fixed Issues</p>
+              <h3 className={classes.cardTitle}>75</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <LocalOffer />
+                Tracked from Github
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="info" stats icon>
+              <CardIcon color="info">
+                <Accessibility />
+              </CardIcon>
+              <p className={classes.cardCategory}>Followers</p>
+              <h3 className={classes.cardTitle}>+245</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <Update />
+                Just Updated
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+      </GridContainer>
+            </>)
+    }
+}
